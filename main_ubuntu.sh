f3f59979ce37d13cf3fc9ed45b5a90f3c88f8a30
ezvk7740 <raveeroj.b@outlook.com>
2021-05-13 16:37:53 +0700

    v1 fixes

--- main_ubuntu.sh
+++ main_ubuntu.sh
@@ -4,7 +4,7 @@ while true
       do
 	  curl -Is http://www.google.com | head -1 | grep 200
 	  if ! [ $? -eq 0 ]; then 
-	      sudo service networking restart
+	      sudo service network-manager restart
 	      sleep 10
 	      sudo -H -u $USER bash -c 'export PATH=$PATH:$PWD && python3 truewifi.py'
 	      echo networking service restarted
